7a181fddffc75b968024db6dbadba9c7cce58294
jpchanson <jan.hanson@techie.com>
2014-11-14 13:48:37 +0000

    icons, and actionBar working - not complete

--- src/jan/hanson/ARC/MainActivity.java
+++ src/jan/hanson/ARC/MainActivity.java
@@ -2,8 +2,9 @@ package jan.hanson.ARC;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.*;
 
-public class MainActivity extends ActionBarActivity
+public class MainActivity extends ActionBarActivity 
 {
     /** Called when the activity is first created. */
     @Override
@@ -12,6 +13,15 @@ public class MainActivity extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.action_bar, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 }
 
 
---
+++ src/jan/hanson/ARC/view/ARCactionBar.java
@@ -0,0 +1,7 @@
+package jan.hanson.ARC.view;
+
+
+
+public class ARCactionBar
+{
+}
